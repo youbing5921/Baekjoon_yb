7213b4e1175b876e8a182e8ed5ca8c76974c18aa
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2025-02-04 00:47:58 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -2,8 +2,18 @@ a = int(input())
 b = input()
 res = 0
 
-for i in range(3):
-    elt = a * int(b[2 - i])
+# for i in range(3):
+#     elt = a * int(b[2 - i])
+#     print(elt)
+#     res += elt * 10 ** i
+
+# print(res)
+
+
+# 디벨롭 : ?자리수 x ?자리수
+b_len = len(b)
+for i in range(b_len):
+    elt = a * int(b[b_len - 1 - i])
     print(elt)
     res += elt * 10 ** i
 
