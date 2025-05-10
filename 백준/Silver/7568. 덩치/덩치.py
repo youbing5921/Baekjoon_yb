5e0cabd00613a263ad786acde13f634728481440
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2025-05-10 17:45:38 +0900

    [Silver V] Title: 덩치, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -2,14 +2,16 @@ n = int(input())
 lst = []
 ans = []
 
+# 데이터 입력 받기
 for _ in range(n):
     lst.append(tuple(map(int, input().split())))
 
-for target_i, target_elt in enumerate(lst):
+for target_i, target_elt in enumerate(lst): # 이 친구의 덩치 등수 찾기
     k = 1
     for i in range(n):
-        if target_i != i:
+        if target_i != i: # 다른 사람과 비교해서
+            # 본인보다 덩치가 큰 사람이라면 등수가 한 칸 내려감.
             if target_elt[0] < lst[i][0] and target_elt[1] < lst[i][1]:
                 k += 1
-    ans.append(k)
+    ans.append(k) # 결과 저장
 print(*ans)
