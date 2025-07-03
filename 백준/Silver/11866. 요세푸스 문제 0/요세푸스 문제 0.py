b3ddc8ea49e44b8a8d7b4fcc55db165c020620ab
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2025-07-04 01:08:07 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,11 +1,11 @@
 # n: 사람 수 / k: 간격
 n, k = map(int, input().split())
-array = list(range(1, n+1))
+array = list(range(1, n+1)) # 사람 테이블 구현
 index = 0
 ans = "<"
 
 while n > 1:
-    index = (index + k-1) % n
+    index = (index + k-1) % n # 제거할 사람의 인덱스 번호 구하기
     ans += str(array.pop(index)) + ", "
     n -= 1
 
