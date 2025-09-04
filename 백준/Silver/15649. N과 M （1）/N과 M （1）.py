e3e682a9e372c7a735e1cd047330ef7bfc4a641b
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2025-09-04 16:37:09 +0900

    [Silver III] Title: N과 M (1), Time: 196 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -6,7 +6,7 @@ def backTracking(perm):
         return
     
     for i in range(1, n+1):
-        if len(perm) > 0 and i in perm: # 가지치기 본인이랑 같은 수가 나오면 패스
+        if len(perm) > 0 and i in perm: # 가지치기 본인이랑 같은 수가 이미 나왔으면 패스
             continue
         else:
             perm.append(i)
@@ -14,3 +14,15 @@ def backTracking(perm):
             perm.pop()
 
 backTracking([])
+
+'''
+# itertools - permutations
+from itertools import permutations
+
+n, m = map(int, input().split())
+lst = list(range(1, n+1))
+ans = permutations(lst, m)
+
+for i in ans:
+    print(*i)
+'''
