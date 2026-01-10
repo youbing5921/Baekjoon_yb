e13dcdee2ef57f123e6055f476e6770947ad4a37
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2026-01-10 16:39:48 +0900

    [level 2] Title: 타겟 넘버, Time: 405.77 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,22 @@
+'''
+# nums들로 res -> t를 만드는 경우의 수 반환
+def make(nums, res, t):
+    if len(nums) == 0:
+        if res == t:
+            return 1
+        else:
+            return 0
+    
+    return make(nums[1:], res + nums[0], t) + make(nums[1:], res - nums[0], t)
+    
 def solution(numbers, target):
-    answer = 0
-    return answer
+    return make(numbers, 0, target) 
+'''
+# 슬라이싱 대신 인덱스 사용
+def solution(numbers, target):
+    def dfs(idx, res):
+        if idx == len(numbers):
+            return res == target
+        return dfs(idx+1, res+numbers[idx]) + dfs(idx+1, res-numbers[idx])
+    
+    return dfs(0, 0)
